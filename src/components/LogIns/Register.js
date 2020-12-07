414aa77122e5b2eae5b005c3eef73934ceeee15a
Aymeric <aymeric.bouault@gmail.com>
2020-12-07 09:40:10 +0100

    creation de la structure du formulaire avec material-ui

--- src/components/LogIns/Register.js
+++ src/components/LogIns/Register.js
@@ -1,9 +1,31 @@
 import React from "react";
+import TextField from '@material-ui/core/TextField';
 
 const Register = () => {
-  return(
-    <div></div>
+  return (
+    <div>
+      <form>
+        <div>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+        <div>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+        <div>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+        <div>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+        <div>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+        <div>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+      </form>
+    </div>
   )
-}
+};
 
 export default Register;
