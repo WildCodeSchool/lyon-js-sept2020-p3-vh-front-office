74e718f1aa884f9a17fbb0cb07272fd0782d701b
Aymeric <aymeric.bouault@gmail.com>
2020-12-07 09:56:48 +0100

    gérer les champs de saisie avec react-hook-form

--- src/components/LogIns/Register.js
+++ src/components/LogIns/Register.js
@@ -1,31 +1,43 @@
 import React from "react";
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
+import { useForm } from "react-hook-form";
+import Button from '@material-ui/core/Button';
 
 const Register = () => {
+
+  const {register, handleSubmit} = useForm()
+
+  const onSubmit = data => {
+    console.log(data)
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="outlined-basic" label="Prénom" variant="outlined" inputRef={register} name="firstname" />
         </div>
         <div>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="outlined-basic" label="Nom" variant="outlined" inputRef={register} name="lastname" />
         </div>
         <div>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="outlined-basic" label="Email" variant="outlined" inputRef={register} name="email" />
         </div>
         <div>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="outlined-basic" label="Téléphone" variant="outlined" inputRef={register} name="phone" />
         </div>
         <div>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="outlined-basic" label="Mot de passe" variant="outlined" inputRef={register} name="password" />
         </div>
         <div>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="outlined-basic" label="Confirmation mot de passe" variant="outlined" inputRef={register} name="confirm" />
+        </div>
+        <div>
+        <Button type="submit" variant="contained" color="primary">Envoyer</Button>
         </div>
       </form>
     </div>
-  )
+  );
 };
 
 export default Register;
