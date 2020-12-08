57fc84bfc4cd9b64cc81e4d320830cbe34c4d439
Aymeric <aymeric.bouault@gmail.com>
2020-12-08 13:06:45 +0100

    fonction pour le changement de langues + événements onClick

--- src/components/Home/Translation.js
+++ src/components/Home/Translation.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useTranslation } from 'react-i18next';
 
 const Translation = () => {
-  return <div>123</div>;
+
+const { t, i18n } = useTranslation();
+
+const handleClickLanguages = (lang) => {
+    i18n.changeLanguage(lang)
+}
+
+  return (
+      <div>
+        <button onClick={() => handleClickLanguages('fr')}>Francais</button>
+        <button onClick={() => handleClickLanguages('en')}>Anglais</button>
+        <p>Bonjour</p>
+      </div>
+  )
 };
 
 export default Translation;
--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,13 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 // don't want to use this?
-// have a look at the Quick start guide 
+// have a look at the Quick start guide
 // for passing in lng and translations on init
 
-const languages = ['fr','en'];
+const Languages = ["fr", "en"];
 
 i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
@@ -21,13 +21,13 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'en',
+    fallbackLng: "en",
     debug: true,
+    whitelist: Languages,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    }
+    },
   });
-
 
 export default i18n;
