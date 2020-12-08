981f5a903d99b8d665052acb135f61852f668ad0
BrieucQ <brieucquertier73@gmail.com>
2020-12-08 22:37:13 +0100

    add scss facebook and google login

--- src/components/LogIns/Login.js
+++ src/components/LogIns/Login.js
@@ -1,24 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import { Link } from 'react-router-dom';
+import "./Login.scss";
+import ReactDOM from 'react-dom';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 function Login() {
-
-
-  const { register, handleSubmit, errors } = useForm({mode: 'onBlur'});
-  const onSubmit = data => console.log(data);
-
+  const { register, handleSubmit, errors } = useForm({ mode: "onBlur" });
+  
+  const onSubmit = () => {
+    // AXIOS et reset form 
+  }
 
   return (
     <>
       <div>
-        <h1>Login</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <img src="/src/logo.jpg"/>
+        </div>
+        <div>
+        <h1 className='login'>Login</h1>
+        </div>
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
           <div>
             <TextField
-            className="is-invalid"
-            name='email'
+              className="is-invalid"
+              name="email"
               inputRef={register({
                 required: "Rentrez votre email",
                 pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
@@ -27,13 +37,11 @@ function Login() {
               label="Email"
               variant="outlined"
             />
-            </div>
-            <div>
-              {errors.email && <span>{errors.email.message}</span>}
-              </div>
+          </div>
+          <div>{errors.email && <span>{errors.email.message}</span>}</div>
           <div>
             <TextField
-            name='password'
+              name="password"
               inputRef={register({
                 required: "Rentrez votre mot de passe",
                 minLength: {
@@ -45,19 +53,34 @@ function Login() {
               id="outlined-basic"
               label="Password"
               variant="outlined"
-              type='password'
+              type="password"
             />
           </div>
-          <div>
-              {errors.password && <span>{errors.password.message}</span>}
-              </div>
+          <div>{errors.password && <span>{errors.password.message}</span>}</div>
           <p>Mot de passe oublié</p>
+          <Link to='/register'>
+          <p>Inscription</p>
+          </Link>
+    
           <div>
             <Button variant="contained" color="primary">
               Login
             </Button>
           </div>
         </form>
+        <div className="facebook_login">
+        <FacebookLogin
+    appId="1088597931155576"
+    autoLoad={true}
+    fields="name,email,picture"/>
+        </div>
+        <div className="google">
+        <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    cookiePolicy={'single_host_origin'}
+  />
+        </div>
       </div>
     </>
   );
