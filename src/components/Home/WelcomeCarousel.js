7b8c865869abf4a1d2a12ed9cc230c13cc48b849
Nicolas Dantas <nicolas.dantas@gmail.com>
2020-12-08 12:18:25 +0100

    alt attribute added

--- src/components/Home/WelcomeCarousel.js
+++ src/components/Home/WelcomeCarousel.js
@@ -12,19 +12,28 @@ const WelcomeCarousel = () => {
       transitionTime={600}
     >
       <div>
-        <img src="https://cdn.pixabay.com/photo/2017/06/26/12/49/red-wine-2443699_960_720.jpg" alt='wine'/>
+        <img
+          src="https://cdn.pixabay.com/photo/2017/06/26/12/49/red-wine-2443699_960_720.jpg"
+          alt="wine"
+        />
         <p className="legend">
           Pariatur do eiusmod laborum sunt in velit et eiusmod ex.
         </p>
       </div>
       <div>
-        <img src="https://cdn.pixabay.com/photo/2015/10/24/11/09/red-wine-1004255_960_720.jpg" alt='wine'/>
+        <img
+          src="https://cdn.pixabay.com/photo/2015/10/24/11/09/red-wine-1004255_960_720.jpg"
+          alt="wine"
+        />
         <p className="legend">
           Pariatur do eiusmod laborum sunt in velit et eiusmod ex.
         </p>
       </div>
       <div>
-        <img src="https://cdn.pixabay.com/photo/2017/06/26/12/49/red-wine-2443699_960_720.jpg" alt='wine'/>
+        <img
+          src="https://cdn.pixabay.com/photo/2017/06/26/12/49/red-wine-2443699_960_720.jpg"
+          alt="wine"
+        />
         <p className="legend">
           Pariatur do eiusmod laborum sunt in velit et eiusmod ex.
         </p>
