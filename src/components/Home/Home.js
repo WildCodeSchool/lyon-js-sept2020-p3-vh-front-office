68924af5928dcada44507c546322123d03cc0cdd
Aymeric <aymeric.bouault@gmail.com>
2020-12-08 17:57:57 +0100

    validations modifications

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Translation from "./Translation";
 
-
 const Home = () => {
   return (
     <div>
--- src/components/Home/Translation.js
+++ src/components/Home/Translation.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import './Translation.scss'
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import "./Translation.scss";
 
 const Translation = () => {
   const { t, i18n } = useTranslation();
@@ -13,13 +13,18 @@ const Translation = () => {
 
   return (
     <div>
-        <div className="container-btn-trad">
-            <ButtonGroup className="btn-trad" disableElevation variant="contained" color="primary">
-                <Button onClick={() => handleClickLanguages("fr")}>fr</Button>
-                <Button onClick={() => handleClickLanguages("en")}>en</Button>
-            </ButtonGroup>
-        </div>
-        <h1 className="title-test-trad">{t("content.title")}</h1>
+      <div className="container-btn-trad">
+        <ButtonGroup
+          className="btn-trad"
+          disableElevation
+          variant="contained"
+          color="primary"
+        >
+          <Button onClick={() => handleClickLanguages("fr")}>fr</Button>
+          <Button onClick={() => handleClickLanguages("en")}>en</Button>
+        </ButtonGroup>
+      </div>
+      <h1 className="title-test-trad">{t("content.title")}</h1>
     </div>
   );
 };
