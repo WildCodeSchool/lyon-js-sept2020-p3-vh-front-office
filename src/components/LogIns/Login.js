58fb57ffe2351dbac1417182f6fec74a967314b2
BrieucQ <brieucquertier73@gmail.com>
2020-12-08 12:34:56 +0100

    Initialisated form login

--- src/components/LogIns/Login.js
+++ src/components/LogIns/Login.js
@@ -1,4 +1,36 @@
 import React from "react";
-export default function Login(props) {
-  return <></>;
+import { useForm } from "react-hook-form";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+function Login() {
+  return (
+    <>
+      <div>
+        <form>
+          <div>
+            <TextField
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <Button variant="contained" color="primary">
+              Login
+            </Button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
+
+export default Login;
