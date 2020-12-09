def8f4ec4f853168ddc4a269867f58ed26eb1417
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-12-09 19:38:54 +0100

    fix linter

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,5 +1,5 @@
-import React from "react";
+import React from 'react';
 
-export default function Footer(props) {
+export default function Footer() {
   return <></>;
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
-import React from "react";
-export default function Home(props) {
+import React from 'react';
+
+export default function Home() {
   return <></>;
 }
