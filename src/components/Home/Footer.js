97e70e2725a68e2585e29ecf9660f78be5a4ed9c
Thibault Verin <thibaultverin@hotmail.com>
2020-12-10 00:56:52 +0100

    Footer responsive, links done

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.scss';
-import logo from '../pictures/hypnose_vins_logo.png';
-import facebook from '../pictures/facebook.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.scss";
+import logo from "../pictures/hypnose_vins_logo.png";
+import facebook from "../pictures/facebook.svg";
 
 const Footer = () => {
   return (
-    <div className='footerBody'>
-      <div className='footerLink'>
+    <div className="footerBody">
+      <div className="footerLink">
         <img
           src={logo}
-          alt='logo'
-          className='footerLogo'
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+          alt="logo"
+          className="footerLogo"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         />
-        <Link to='/contact'>
+        <Link to="/contact">
           <p>Contact</p>
         </Link>
-        <Link to='faq'>
+        <Link to="faq">
           <p>FAQ</p>
         </Link>
-        <Link to='mentions-legales'>
-          {' '}
+        <Link to="mentions-legales">
+          {" "}
           <p>Mentions légales</p>
         </Link>
-        <img src={facebook} alt='facebookIcon' className='footerFacebook' />
+        <img src={facebook} alt="facebookIcon" className="footerFacebook" />
       </div>
 
-      <div className='footerCopyright'>
+      <div className="footerCopyright">
         <p>Wild Code School © 2020. All rights reserved</p>
       </div>
     </div>
