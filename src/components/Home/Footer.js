aa43ae9ef673151ca9e545a319ded43b429ac8ff
Thibault Verin <thibaultverin@hotmail.com>
2020-12-09 11:01:39 +0100

    Creation variable CSS, picture folder, Footer mobile almost done

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.scss';
+import logo from '../pictures/hypnose_vins_logo.png';
+import facebook from '../pictures/facebook.svg';
 
-export default function Footer(props) {
-  return <></>;
-}
+const Footer = () => {
+  return (
+    <div className='footerBody'>
+      <img src={logo} alt='logo' className='footerLogo' />
+      <div className='footerText'>
+        <div className='footerLink'>
+          <Link to='/contact'>
+            <p>Contact</p>
+          </Link>
+          <Link to='faq'>
+            <p>FAQ</p>
+          </Link>
+          <Link to='mentions-legales'>
+            {' '}
+            <p>Mentions légales</p>
+          </Link>
+        </div>
+        <div className='footerCopyright'>
+          <p>Lorem ipsum</p>
+        </div>
+      </div>
+      <img src={facebook} alt='facebookIcon' className='footerFacebook' />
+    </div>
+  );
+};
+
+export default Footer;
