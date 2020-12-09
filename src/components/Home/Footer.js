71583a6e33f8a71d01b11e0f6fb96d938c57ff6e
Thibault Verin <thibaultverin@hotmail.com>
2020-12-10 00:56:34 +0100

    Footer responsive, links done

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,31 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Footer.scss";
-import logo from "../pictures/hypnose_vins_logo.png";
-import facebook from "../pictures/facebook.svg";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.scss';
+import logo from '../pictures/hypnose_vins_logo.png';
+import facebook from '../pictures/facebook.svg';
 
 const Footer = () => {
   return (
-    <div className="footerBody">
-      <img src={logo} alt="logo" className="footerLogo" />
-      <div className="footerText">
-        <div className="footerLink">
-          <Link to="/contact">
-            <p>Contact</p>
-          </Link>
-          <Link to="faq">
-            <p>FAQ</p>
-          </Link>
-          <Link to="mentions-legales">
-            {" "}
-            <p>Mentions légales</p>
-          </Link>
-        </div>
-        <div className="footerCopyright">
-          <p>Lorem ipsum</p>
-        </div>
+    <div className='footerBody'>
+      <div className='footerLink'>
+        <img
+          src={logo}
+          alt='logo'
+          className='footerLogo'
+          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        />
+        <Link to='/contact'>
+          <p>Contact</p>
+        </Link>
+        <Link to='faq'>
+          <p>FAQ</p>
+        </Link>
+        <Link to='mentions-legales'>
+          {' '}
+          <p>Mentions légales</p>
+        </Link>
+        <img src={facebook} alt='facebookIcon' className='footerFacebook' />
       </div>
-      <img src={facebook} alt="facebookIcon" className="footerFacebook" />
+
+      <div className='footerCopyright'>
+        <p>Wild Code School © 2020. All rights reserved</p>
+      </div>
     </div>
   );
 };
