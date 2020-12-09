f8eca7f1b65e43aa0aa6e535f875459daf0cf195
Thibault Verin <thibaultverin@hotmail.com>
2020-12-09 11:06:42 +0100

    Creation variable CSS, picture folder, Footer mobile almost done

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.scss';
-import logo from '../pictures/hypnose_vins_logo.png';
-import facebook from '../pictures/facebook.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.scss";
+import logo from "../pictures/hypnose_vins_logo.png";
+import facebook from "../pictures/facebook.svg";
 
 const Footer = () => {
   return (
-    <div className='footerBody'>
-      <img src={logo} alt='logo' className='footerLogo' />
-      <div className='footerText'>
-        <div className='footerLink'>
-          <Link to='/contact'>
+    <div className="footerBody">
+      <img src={logo} alt="logo" className="footerLogo" />
+      <div className="footerText">
+        <div className="footerLink">
+          <Link to="/contact">
             <p>Contact</p>
           </Link>
-          <Link to='faq'>
+          <Link to="faq">
             <p>FAQ</p>
           </Link>
-          <Link to='mentions-legales'>
-            {' '}
+          <Link to="mentions-legales">
+            {" "}
             <p>Mentions légales</p>
           </Link>
         </div>
-        <div className='footerCopyright'>
+        <div className="footerCopyright">
           <p>Lorem ipsum</p>
         </div>
       </div>
-      <img src={facebook} alt='facebookIcon' className='footerFacebook' />
+      <img src={facebook} alt="facebookIcon" className="footerFacebook" />
     </div>
   );
 };
