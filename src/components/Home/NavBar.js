81207839694ce6e9780ef19472f2210698bd8432
Thibault Verin <thibaultverin@hotmail.com>
2020-12-10 01:16:30 +0100

    creation navbar branch

--- src/components/Home/NavBar.js
+++ src/components/Home/NavBar.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.scss';
+import logo from '../pictures/hypnose_vins_logo.png';
+import { slide as Menu } from 'react-burger-menu';
 
-export default function NavBar(props) {
-  return <></>;
-}
+const NavBar = () => {
+  return (
+    <div className='navbarBody'>
+      <img
+        src={logo}
+        alt='logo hypnose and wine'
+        className='navbarLogo'
+        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+      />
+      <div className='navbarLink'>
+        <Link to='/contact'>
+          <p>Contact</p>
+        </Link>
+        <Link to='/contact'>
+          <p>Contact</p>
+        </Link>
+        <Link to='/contact'>
+          <p>Contact</p>
+        </Link>
+        <Link to='/contact'>
+          <p></p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
