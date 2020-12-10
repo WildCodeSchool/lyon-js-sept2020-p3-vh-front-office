87f80b902305204c20df49d603e9db29aa52f4b8
Nicolas Dantas <nicolas.dantas@gmail.com>
2020-12-10 17:36:28 +0100

    fix bug in scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home/Home';
 import AboutMorgane from './About/AboutMorgane';
