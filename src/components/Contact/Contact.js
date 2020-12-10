ca5e4425bae9f0fb0bd066183a2872739befe7b7
LaurencePortron <portron.laurence@gmail.com>
2020-12-10 16:35:47 +0100

    reset form done, first css iteration done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import './Contact.scss';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./Contact.scss";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Button from "@material-ui/core/Button";
 
-import _ from 'lodash/fp';
+import _ from "lodash/fp";
 
 const useStyles = makeStyles(() => ({
   root: {
-    '& > *': {
-      width: '25ch',
+    "& > *": {
+      width: "25ch",
     },
   },
   formControl: {
@@ -22,10 +22,10 @@ const useStyles = makeStyles(() => ({
   },
 
   btn: {
-    backgroundColor: '#6d071a',
-    textTransform: 'none',
-    '&:hover': {
-      backgroundColor: '#6d071a',
+    backgroundColor: "#6d071a",
+    textTransform: "none",
+    "&:hover": {
+      backgroundColor: "#6d071a",
     },
   },
 }));
@@ -40,7 +40,7 @@ const Contact = () => {
   const classes = useStyles();
 
   const { register, handleSubmit, formState, errors } = useForm({
-    mode: 'onBlur',
+    mode: "onBlur",
   });
 
   const { isSubmitting, isSubmitSuccessful } = formState;
@@ -52,118 +52,118 @@ const Contact = () => {
   };
 
   return (
-    <div className='container-register-form'>
-      <form className='contactForm' onSubmit={handleSubmit(onSubmit)}>
+    <div className="container-register-form">
+      <form className="contactForm" onSubmit={handleSubmit(onSubmit)}>
         <h1>Contactez Nous</h1>
 
-        <div className='input-register-form'>
-          <div className='contactErrorMessage'>
+        <div className="input-register-form">
+          <div className="contactErrorMessage">
             {errors.lastname && <span>{errors.lastname.message}</span>}
           </div>
           <TextField
             className={classes.input}
-            id='outlined-basic'
-            label='Nom'
-            name='lastname'
-            variant='outlined'
+            id="outlined-basic"
+            label="Nom"
+            name="lastname"
+            variant="outlined"
             inputRef={register({
-              required: 'Champ obligatoire',
+              required: "Champ obligatoire",
             })}
           />
         </div>
 
-        <div className='input-register-form'>
-          <div className='contactErrorMessage'>
+        <div className="input-register-form">
+          <div className="contactErrorMessage">
             {errors.firstname && <span>{errors.firstname.message}</span>}
           </div>
           <TextField
             className={classes.input}
-            id='outlined-basic'
-            label='Prénom'
-            name='firstname'
-            variant='outlined'
+            id="outlined-basic"
+            label="Prénom"
+            name="firstname"
+            variant="outlined"
             inputRef={register({
-              required: 'Champ obligatoire',
+              required: "Champ obligatoire",
               maxLength: {
                 value: 20,
-                message: 'Maximum 20 caractères',
+                message: "Maximum 20 caractères",
               },
             })}
           />
         </div>
 
-        <div className='input-register-form'>
-          <div className='contactErrorMessage'>
+        <div className="input-register-form">
+          <div className="contactErrorMessage">
             {errors.email && <span>{errors.email.message}</span>}
           </div>
           <TextField
             className={classes.input}
-            id='outlined-basic'
-            label='Email'
-            name='email'
-            variant='outlined'
+            id="outlined-basic"
+            label="Email"
+            name="email"
+            variant="outlined"
             inputRef={register({
-              required: 'Champ obligatoire',
+              required: "Champ obligatoire",
               pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-              message: 'Email invalide',
+              message: "Email invalide",
             })}
           />
         </div>
 
-        <div className='input-register-form'>
+        <div className="input-register-form">
           <FormControl className={classes.formControl}>
-            <InputLabel id='demo-simple-select-label'>
+            <InputLabel id="demo-simple-select-label">
               Motif de Contact
             </InputLabel>
             <Select
               className={classes.input}
-              labelId='demo-simple-select-label'
-              id='demo-simple-select'
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
               inputRef={register({
-                required: 'Champ obligatoire',
-                message: 'Email invalide',
+                required: "Champ obligatoire",
+                message: "Email invalide",
               })}
             >
-              <MenuItem value={'client'}>
+              <MenuItem value={"client"}>
                 Question/Participation Evènement
               </MenuItem>
-              <MenuItem value={'partenaire'}>Partenariat</MenuItem>
-              <MenuItem value={'animateur'}>Animer un Evènement</MenuItem>
+              <MenuItem value={"partenaire"}>Partenariat</MenuItem>
+              <MenuItem value={"animateur"}>Animer un Evènement</MenuItem>
             </Select>
           </FormControl>
         </div>
-        <div className='input-register-form'>
-          <div className='contactErrorMessage'>
+        <div className="input-register-form">
+          <div className="contactErrorMessage">
             {errors.message && <span>{errors.message.message}</span>}
           </div>
           <TextField
-            id='outlined-basic'
+            id="outlined-basic"
             className={classes.input}
-            label='Message'
+            label="Message"
             multiline
             rows={4}
-            name='message'
-            variant='outlined'
+            name="message"
+            variant="outlined"
             inputRef={register({
-              required: 'Champ obligatoire',
+              required: "Champ obligatoire",
             })}
           />
         </div>
 
-        <div className='button-register-form'>
+        <div className="button-register-form">
           <Button
             className={classes.btn}
             disableElevation={isSubmitting}
-            type='submit'
-            variant='contained'
-            color='primary'
+            type="submit"
+            variant="contained"
+            color="primary"
           >
             Envoyer
           </Button>
         </div>
-        <div className='button-register-form'>
+        <div className="button-register-form">
           {isSubmitSuccessful && (
-            <div className='message-envoie-form'>
+            <div className="message-envoie-form">
               <p>Merci pour votre inscription</p>
             </div>
           )}
