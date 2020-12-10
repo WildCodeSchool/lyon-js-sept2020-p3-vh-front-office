67e7e5a74b943961d258aa6f1dc9af1352b39a01
KarenOrduna <kv8orduna@gmail.com>
2020-12-10 14:16:08 +0100

    created Morgane's presentation page

--- src/components/About/AboutMorgane.js
+++ src/components/About/AboutMorgane.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 import {
   AiOutlineMail,
   AiFillFacebook,
   AiOutlineTwitter,
   AiOutlineInstagram,
-} from 'react-icons/ai';
-import './AboutMorgane.scss';
+} from "react-icons/ai";
+import "./AboutMorgane.scss";
 
 export default function AboutMorgane() {
   return (
     <div>
-      <h1 className='titleAbout'> Presentation Morgane</h1>
-      <div className='squareAboutGrey'>
-        <div className='aboutTitle'>
-          <h2 className='whoTitle'>Qui suis je?</h2>
+      <h1 className="titleAbout"> Presentation Morgane</h1>
+      <div className="squareAboutGrey">
+        <div className="aboutTitle">
+          <h2 className="whoTitle">Qui suis je?</h2>
         </div>
-        <div className='aboutImage'>
+        <div className="aboutImage">
           <img
-            className='imageMorgane'
-            alt=''
-            src='https://www.pngfind.com/pngs/m/5-52768_girl-avatar-download-transparent-png-image-png-download.png'
-            width='200'
-            height='200'
+            className="imageMorgane"
+            alt=""
+            src="https://www.pngfind.com/pngs/m/5-52768_girl-avatar-download-transparent-png-image-png-download.png"
+            width="200"
+            height="200"
           />
-          <div className='autoBio'>
+          <div className="autoBio">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -35,26 +35,26 @@ export default function AboutMorgane() {
             </p>
           </div>
         </div>
-        <div className='socialMedia'>
-          <ul class='icon-bar'>
-            <li className='list'>
-              <a href='http://www.google.com'>
-                <AiOutlineMail size={32} color={'black'} />
+        <div className="socialMedia">
+          <ul class="icon-bar">
+            <li className="list">
+              <a href="http://www.google.com">
+                <AiOutlineMail size={32} color={"black"} />
               </a>
             </li>
-            <li className='list'>
-              <a href='http://www.google.com'>
-                <AiFillFacebook size={32} color={'black'} />
+            <li className="list">
+              <a href="http://www.google.com">
+                <AiFillFacebook size={32} color={"black"} />
               </a>
             </li>
-            <li className='list'>
-              <a href='http://www.google.com'>
-                <AiOutlineTwitter size={32} color={'black'} />
+            <li className="list">
+              <a href="http://www.google.com">
+                <AiOutlineTwitter size={32} color={"black"} />
               </a>
             </li>
-            <li className='list'>
-              <a href='http://www.google.com'>
-                <AiOutlineInstagram size={32} color={'black'} />
+            <li className="list">
+              <a href="http://www.google.com">
+                <AiOutlineInstagram size={32} color={"black"} />
               </a>
             </li>
           </ul>
