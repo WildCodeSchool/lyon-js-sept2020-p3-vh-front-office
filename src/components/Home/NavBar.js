36424e1a9469ab622954316d24d8c589aef9a067
Thibault Verin <thibaultverin@hotmail.com>
2020-12-10 09:26:17 +0100

    Few CSS changes

--- src/components/Home/NavBar.js
+++ src/components/Home/NavBar.js
@@ -1,32 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./NavBar.scss";
-import logo from "../pictures/hypnose_vins_logo.png";
-import { slide as Menu } from "react-burger-menu";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.scss';
+import logo from '../pictures/hypnose_vins_logo.png';
+import { slide as Menu } from 'react-burger-menu';
 
 const NavBar = () => {
   return (
-    <div className="navbarBody">
+    <div className='navbarBody'>
       <img
         src={logo}
-        alt="logo hypnose and wine"
-        className="navbarLogo"
-        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        alt='logo hypnose and wine'
+        className='navbarLogo'
+        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
       />
-      <div className="navbarLink">
-        <Link to="/contact">
-          <p>Contact</p>
-        </Link>
-        <Link to="/contact">
-          <p>Contact</p>
-        </Link>
-        <Link to="/contact">
-          <p>Contact</p>
-        </Link>
-        <Link to="/contact">
-          <p></p>
-        </Link>
-      </div>
     </div>
   );
 };
