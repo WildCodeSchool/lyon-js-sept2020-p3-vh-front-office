1dd26998e5a1f5575e5b124e4a8ba33c0fb5558b
Thibault Verin <thibaultverin@hotmail.com>
2020-12-10 02:09:05 +0100

    Css fixes

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.scss';
-import logo from '../pictures/hypnose_vins_logo.png';
-import facebook from '../pictures/facebook.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.scss";
+import logo from "../pictures/hypnose_vins_logo.png";
+import facebook from "../pictures/facebook.svg";
 
 const Footer = () => {
   return (
-    <div className='footerBody'>
+    <div className="footerBody">
       <img
         src={logo}
-        alt='logo'
-        className='footerLogo'
-        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        alt="logo"
+        className="footerLogo"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
       />
-      <div className='footerText'>
-        <div className='footerLink'>
-          <Link to='/contact'>
+      <div className="footerText">
+        <div className="footerLink">
+          <Link to="/contact">
             <p>Contact</p>
           </Link>
-          <Link to='faq'>
+          <Link to="faq">
             <p>FAQ</p>
           </Link>
-          <Link to='mentions-legales'>
-            {' '}
+          <Link to="mentions-legales">
+            {" "}
             <p>Mentions légales</p>
           </Link>
         </div>
-        <div className='footerCopyright'>
+        <div className="footerCopyright">
           <p>Wild Code School © 2020. All rights reserved</p>
         </div>
       </div>
-      <div className='footerIcon'>
-        <img src={facebook} alt='facebookIcon' className='footerFacebook' />
+      <div className="footerIcon">
+        <img src={facebook} alt="facebookIcon" className="footerFacebook" />
       </div>
     </div>
   );
