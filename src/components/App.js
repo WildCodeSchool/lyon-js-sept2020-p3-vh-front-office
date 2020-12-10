33943334bae0c56bb0461ae6a35aca432a65e08d
Nicolas Dantas <nicolas.dantas@gmail.com>
2020-12-10 18:34:48 +0100

    fixed an other bug with react bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import './App.scss';
+import './bootstrap.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home/Home';
 import AboutMorgane from './About/AboutMorgane';
