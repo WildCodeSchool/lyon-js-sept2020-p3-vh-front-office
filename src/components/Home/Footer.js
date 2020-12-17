47c05574381b2a39231e51d56faf826d3ca1e11a
Thibault Verin <thibaultverin@hotmail.com>
2020-12-17 11:59:55 +0100

    Start working on new footer design

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './Footer.scss';
-import logo from '../pictures/hypnose_vins_logo_icone.png';
-import facebook from '../pictures/facebook.svg';
+// import logo from '../pictures/hypnose_vins_logo_icone.png';
+// import facebook from '../pictures/facebook.svg';
 
 const Footer = () => {
   return (
     <footer className="footerBody">
-      <Link to="/">
-        <img src={logo} alt="logo" className="footerLogo" />
-      </Link>
-      <div className="footerText">
-        <div className="footerLink">
-          <Link to="/contact">
-            <p>Contact</p>
-          </Link>
-          <Link to="faq">
-            <p>FAQ</p>
-          </Link>
-          <Link to="mentions-legales">
-            {' '}
-            <p>Mentions légales</p>
-          </Link>
+      <div className="footerTop">
+        <div className="footerSignUp">
+          <p>Sign up for upcoming events</p>
         </div>
-        <div className="footerCopyright">
-          <p>Wild Code School © 2020. All rights reserved</p>
-        </div>
-      </div>
-      <div className="footerIcon">
-        <img src={facebook} alt="facebookIcon" className="footerFacebook" />
+        <div className="footerMainLink">Hello bicth </div>
       </div>
     </footer>
   );
