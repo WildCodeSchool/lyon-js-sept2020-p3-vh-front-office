2fc3a2265e33746224838162195c8f091b55e2a2
LaurencePortron <portron.laurence@gmail.com>
2020-12-19 09:46:03 +0100

    ratings page started

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import './Reviews.css';
+import { User } from 'react-feather';
 
 export default function Reviews() {
-  return <></>;
+  return (
+    <>
+      <h1>Ratings</h1>
+
+      <div className="stars">&#9733; &#9733; &#9733; &#9733;&#9733;</div>
+      <div className="cards">
+        <div className="avatar-text">
+          <div className="avatar">
+            <User width="80" />
+          </div>
+          <div className="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+        </div>
+        <div className="stars">&#9733; &#9733; &#9733; &#9733;&#9733;</div>
+      </div>
+    </>
+  );
 }
