2a833797d147dd2d2dd8ddb7d13295eb41517523
Aymeric <aymeric.bouault@gmail.com>
2020-12-21 16:03:32 +0100

    cabler la table animators avec le front + map pour recupérer les infos de la BDD

--- src/components/About/AboutAnimators.js
+++ src/components/About/AboutAnimators.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import API from '../../services/API';
 
-export default function AboutAnimators() {
-  return <></>;
-}
+const AboutAnimators = () => {
+  const [animators, setAnimators] = useState('');
+
+  useEffect(() => {
+    API.get('/animators').then((res) => setAnimators(res.data));
+  }, [animators]);
+
+  return (
+    <div>
+      {animators &&
+        animators.map((animator) => {
+          return (
+            <div>
+              <h1>{animator.firstname}</h1>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default AboutAnimators;
