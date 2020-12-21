4787f4fdc1763fa23e2090b8b48916a0a2d2e98b
Aymeric <aymeric.bouault@gmail.com>
2020-12-21 17:06:43 +0100

    Début du CSS pour la page des Animateurs

--- src/components/About/AboutAnimators.js
+++ src/components/About/AboutAnimators.js
@@ -1,20 +1,67 @@
 import React, { useState, useEffect } from 'react';
+import {
+  AiOutlineMail,
+  AiFillFacebook,
+  AiOutlineTwitter,
+  AiOutlineInstagram,
+} from 'react-icons/ai';
 import API from '../../services/API';
+import './AboutAnimators.scss';
 
 const AboutAnimators = () => {
-  const [animators, setAnimators] = useState('');
+  const [animators, setAnimators] = useState();
 
   useEffect(() => {
     API.get('/animators').then((res) => setAnimators(res.data));
   }, [animators]);
 
   return (
-    <div>
+    <div className="container-all-cards-animators">
+      <h1>Animateurs</h1>
       {animators &&
         animators.map((animator) => {
           return (
-            <div>
-              <h1>{animator.firstname}</h1>
+            <div className="cards-animators">
+              <div className="nom-image-animator">
+                <h2>
+                  {animator.firstname}
+                  {animator.lastname}
+                </h2>
+                <img src={animator.image} alt={animator.firstname} />
+              </div>
+              <p>{animator.biographie}</p>
+              <div className="socialMedia">
+                <ul className="icon-bar">
+                  <div>
+                    <li className="list">
+                      <a href="http://www.google.com">
+                        <AiOutlineMail size={32} color="black" />
+                      </a>
+                    </li>
+                  </div>
+                  <div>
+                    <li className="list">
+                      <a href="http://www.google.com">
+                        <AiFillFacebook size={32} color="black" />
+                      </a>
+                    </li>
+                  </div>
+                  <div>
+                    <li className="list">
+                      <a href="http://www.google.com">
+                        <AiOutlineTwitter size={32} color="black" />
+                      </a>
+                    </li>
+                  </div>
+                  <div>
+                    <li className="list">
+                      <a href="http://www.google.com">
+                        <AiOutlineInstagram size={32} color="black" />
+                      </a>
+                    </li>
+                  </div>
+                </ul>
+              </div>
             </div>
           );
         })}
