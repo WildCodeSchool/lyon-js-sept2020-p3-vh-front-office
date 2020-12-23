3eecc78cd15d3823535040598196fccfc2eb8b37
BrieucQ <brieucquertier73@gmail.com>
2020-12-23 19:06:42 +0100

    first modification front event

--- src/components/Events/Eventdetails.js
+++ src/components/Events/Eventdetails.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './Eventdetail.scss';
 
 export default function EventDetails() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1 className="main-title">Réserver un évènement</h1>
+      </div>
+    </>
+  );
 }
