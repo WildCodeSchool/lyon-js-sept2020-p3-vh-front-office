67a219c971f14f4089bca2933093edb7662ace54
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-01-05 17:54:28 +0100

    local storage implemented to make basket persisting

--- src/components/Contexts/BasketContext.js
+++ src/components/Contexts/BasketContext.js
@@ -1,9 +1,9 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const BasketContext = createContext();
 
 export default function BasketProvider({ children }) {
-  const [basket, setBasket] = useState([
+  const simulatedBasket = [
     {
       id: 1,
       quantity: 4,
@@ -12,7 +12,21 @@ export default function BasketProvider({ children }) {
       id: 2,
       quantity: 1,
     },
-  ]);
+  ];
+
+  const [basket, setBasket] = useState(
+    JSON.parse(localStorage.getItem('events')) || simulatedBasket
+  );
+
+  // We'll use this useState when booking events feature will be developped
+
+  // const [basket, setBasket] = useState(
+  //   JSON.parse(localStorage.getItem('events')) || simulatedBasket
+  // );
+
+  useEffect(() => {
+    localStorage.setItem('events', JSON.stringify(basket));
+  }, [basket]);
 
   return (
     <BasketContext.Provider value={{ basket, setBasket }}>
