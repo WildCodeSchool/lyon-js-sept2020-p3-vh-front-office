998e2adc14037fd36f30c4bb474c3ab65b93dcfe
Thibault Verin <thibaultverin@hotmail.com>
2021-01-05 10:28:35 +0100

    cablage route event

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Events.scss';
+import API from '../../services/API';
 
 const Events = () => {
-  return <div>Hello Bitch</div>;
+  const [events, setEvents] = useState();
+
+  useEffect(() => {
+    API.get('/events').then((res) => setEvents(res.data));
+  }, [events]);
+
+  return (
+    <div className="eventBody">
+      {events &&
+        events.map((event) => {
+          return (
+            <div className="event">
+              <div className="eventCard">
+                <img src={event.main_picture_url} alt={event.title} />
+                <div>
+                  <h2>{event.title}</h2>
+                  <p>{event.description}</p>
+                </div>
+              </div>
+              <div className="underCard">
+                <p>{event.address_id}</p>
+                <p>{event.moderator_id}</p>
+                <p>{event.wine_id}</p>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Events;
