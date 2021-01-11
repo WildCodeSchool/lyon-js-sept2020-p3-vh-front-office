c1abd28d418ee41339f02f3981bfe94a2d22b0a4
KarenOrduna <kv8orduna@gmail.com>
2021-01-11 15:46:22 +0100

    front part of the Calendar done

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,17 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Calendar from 'react-calendar';
+import './Calendar.scss';
+import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
 import './Events.scss';
 import API from '../../services/API';
 
 const Events = () => {
   const [events, setEvents] = useState();
+  const [value, onChange] = useState(new Date());
+  const [currentDate, setCurrentDate] = useState(new Date());
 
   useEffect(() => {
-    API.get('/events').then((res) => setEvents(res.data));
-  }, []);
+    const currentDateString = moment(currentDate).format('YYYY-MM-DD');
+    API.get(
+      `/events?after=${currentDateString}&before=${currentDateString}`
+    ).then((res) => setEvents(res.data));
+    console.log(currentDateString);
+  }, [currentDate]);
 
   return (
     <div className="eventBody">
+      <div className="calendar-container">
+        <Calendar
+          onChange={onChange}
+          value={value}
+          onClickDay={setCurrentDate}
+          onClickWeekNumber={currentDate}
+        />
+      </div>
       {events &&
         events.map((event) => {
           return (
