82b01d17b74430abc12eb13976381e05907f5189
LaurencePortron <portron.laurence@gmail.com>
2021-01-11 15:33:32 +0100

    first version of ratings done

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,14 +1,10 @@
+/* eslint-disable no-nested-ternary */
 import React, { useState, useEffect } from 'react';
 
 import './Reviews.css';
-import { User } from 'react-feather';
-// import Rating from '@material-ui/lab/Rating';
-// import Box from '@material-ui/core/Box';
 import API from '../../services/API';
 
 export default function Reviews() {
-  // const [value, setValue] = useState();
-
   const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
@@ -21,49 +17,31 @@ export default function Reviews() {
     <>
       <h1>Témoignages</h1>
 
-      {/* <Box
-        className="box"
-        component="fieldset"
-        mb={3}
-        borderColor="transparent"
-      >
-        <Rating
-          name="read-only"
-          value={value}
-          readOnly
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-        />
-      </Box> */}
-
       <div>
         {reviews.map((review) => {
           return (
             <div className="cards">
-              <div className="avatar-text">
-                <div className="avatar">
-                  <User width="80" />
-                </div>
-                <div key={review.id}>
-                  <p className="text">{review.comment}</p>
-                  <p className="test">{review.rating}</p>
+              <div key={review.id} className="ratings-comments">
+                <p className="comments"> {review.comment}</p>
+                <div className="ratings">
+                  {review.rating === 5 ? (
+                    <p>&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                  ) : review.rating === 4 ? (
+                    <p>&#9733;&#9733;&#9733;&#9733;</p>
+                  ) : review.rating === 3 ? (
+                    <p>&#9733;&#9733;&#9733;</p>
+                  ) : review.rating === 2 ? (
+                    <p>&#9733;&#9733;</p>
+                  ) : review.rating === 1 ? (
+                    <p>&#9733;</p>
+                  ) : (
+                    ''
+                  )}
                 </div>
               </div>
             </div>
           );
         })}
-
-        {/* <Box component="fieldset" mb={3} borderColor="transparent">
-          <Rating
-            name="read-only"
-            value={value}
-            readOnly
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-          />
-        </Box> */}
       </div>
     </>
   );
