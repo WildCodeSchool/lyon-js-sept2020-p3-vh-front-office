2247704ae08e00fd09d6a4d292a171b94f1eefb7
Aymeric <aymeric.bouault@gmail.com>
2021-01-20 14:15:35 +0100

    cablage avec le back + affichage rating avec material ui

--- src/components/Home/Review.js
+++ src/components/Home/Review.js
@@ -1,32 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Review.scss';
+import Rating from '@material-ui/lab/Rating';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { getCollection } from '../../services/API';
 
 function Review() {
-  const sliderArr = [1, 2, 3, 4, 5];
+  // eslint-disable-next-line no-unused-vars
+  const [reviews, setReviews] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [rating, setRating] = useState(2);
+  /*   const sliderArr = [1, 2, 3, 4, 5]; */
   const [changeSlide, setChangeSlide] = useState(0);
   const handleClicLeft = () => {
     // eslint-disable-next-line no-unused-expressions
     changeSlide === 0
-      ? setChangeSlide(-100 * (sliderArr.length - 1))
+      ? setChangeSlide(-100 * (reviews.length - 1))
       : setChangeSlide(changeSlide + 100);
   };
   const handleClicRight = () => {
     // eslint-disable-next-line no-unused-expressions
-    changeSlide === -100 * (sliderArr.length - 1)
+    changeSlide === -100 * (reviews.length - 1)
       ? setChangeSlide(0)
       : setChangeSlide(changeSlide - 100);
   };
 
+  useEffect(() => {
+    const request = getCollection('reviews').then((data) => setReviews(data));
+    return () => {
+      request.cancel();
+    };
+  }, []);
+
   return (
-    <div>
+    <div className="container-carousel-reviews">
       <div className="slider">
-        {sliderArr.map((item) => {
+        {reviews.map((review) => {
           return (
             <div
               className="slide-reviews"
               style={{ transform: `translateX(${changeSlide}%)` }}
             >
-              {item}
+              <p>{review.comment}</p>
+              <Box component="fieldset" mb={3} borderColor="transparent">
+                <Typography component="legend" />
+                <Rating name="read-only" value={review.rating} readOnly />
+              </Box>
             </div>
           );
         })}
@@ -34,7 +53,7 @@ function Review() {
           <i className="fa fa-chevron-left" aria-hidden="true" />
         </button>
         <button type="button" id="goRight" onClick={handleClicRight}>
-          <i className="fa fa-chevron-left" aria-hidden="true" />
+          <i className="fa fa-chevron-right" aria-hidden="true" />
         </button>
       </div>
     </div>
