16d36929cd158c080934faafe9ee215a1b60ee35
KarenOrduna <kv8orduna@gmail.com>
2021-01-21 14:45:34 +0100

    fixed css

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -95,56 +95,58 @@ const Events = () => {
           Réinitialiser
         </Button>
       </div>
-      <Helmet>
-        <title>Événements</title>
-      </Helmet>
-      {events &&
-        events.map((event) => {
-          return (
-            <div className="eventCard" key={event.id}>
-              <div className="eventDetail">
-                <img src={event.main_picture_url} alt={event.title} />
-                <div className="eventDescription">
-                  <h3>{event.title}</h3>
-                  <p>{event.description}</p>
-                  <div className="button">
-                    <div className="share">
-                      <FacebookShareButton
-                        className="facebook"
-                        url="https://www.youtube.com/"
-                      >
-                        <FacebookIcon size={30} borderRadius={50}>
-                          Facebook
-                        </FacebookIcon>
-                      </FacebookShareButton>
+      <div className="cardOfEvents">
+        <Helmet>
+          <title>Événements</title>
+        </Helmet>
+        {events &&
+          events.map((event) => {
+            return (
+              <div className="eventCard" key={event.id}>
+                <div className="eventDetail">
+                  <img src={event.main_picture_url} alt={event.title} />
+                  <div className="eventDescription">
+                    <h3>{event.title}</h3>
+                    <p>{event.description}</p>
+                    <div className="button">
+                      <div className="share">
+                        <FacebookShareButton
+                          className="facebook"
+                          url="https://www.youtube.com/"
+                        >
+                          <FacebookIcon size={30} borderRadius={50}>
+                            Facebook
+                          </FacebookIcon>
+                        </FacebookShareButton>
 
-                      <TwitterShareButton
-                        className="twitter"
-                        url="https://twitter.com/"
-                      >
-                        <TwitterIcon size={30} borderRadius={50}>
-                          Twitter
-                        </TwitterIcon>
-                      </TwitterShareButton>
+                        <TwitterShareButton
+                          className="twitter"
+                          url="https://twitter.com/"
+                        >
+                          <TwitterIcon size={30} borderRadius={50}>
+                            Twitter
+                          </TwitterIcon>
+                        </TwitterShareButton>
+                      </div>
+                      <Link to={`/events/${event.id}`}>
+                        <button className="reserver" type="button">
+                          Réserver
+                        </button>
+                      </Link>
                     </div>
-                    <Link to={`/events/${event.id}`}>
-                      <button className="reserver" type="button">
-                        Réserver
-                      </button>
-                    </Link>
                   </div>
                 </div>
+                <div className="underCard">
+                  <p>{event.city}</p>
+                  <p>
+                    {event.firstname} {event.lastname}
+                  </p>
+                  <p>{event.name}</p>
+                </div>
               </div>
-              <div className="underCard">
-                <p>{event.city}</p>
-                <p>
-                  {event.firstname} {event.lastname}
-                </p>
-                <p>{event.name}</p>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
     </div>
   );
 };
