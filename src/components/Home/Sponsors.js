b826542899af68f6efdfbe2b7cc8e95374d474b0
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-01-22 15:32:09 +0100

    did some style

--- src/components/Home/Sponsors.js
+++ src/components/Home/Sponsors.js
@@ -1,37 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import { getCollection } from '../../services/API';
 import './Sponsors.scss';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 300,
-  },
-  media: {
-    height: 0,
-    paddingTop: '100%', // 16:9
-    borderRadius: '50%',
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-}));
-
 const Sponsors = () => {
-  const classes = useStyles();
-
   const [sponsors, setSponsors] = useState();
 
   useEffect(() => {
@@ -42,25 +13,21 @@ const Sponsors = () => {
   }, []);
 
   return (
-    <div className="cards">
-      {sponsors &&
-        sponsors.map((sponsor) => {
-          return (
-            <Card key={sponsor.id} className={classes.root}>
-              <CardHeader title="One of the best software ever" />
-              <CardMedia
-                className={classes.media}
-                image={sponsor.image}
-                title="Do you want use this fucking software ?"
-              />
-              <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Do you want use this fucking software ?
-                </Typography>
-              </CardContent>
-            </Card>
-          );
-        })}
+    <div className="sponsors-main">
+      <h1>Nos partenaires :</h1>
+      <div className="cards">
+        {sponsors &&
+          sponsors.map((sponsor) => {
+            return (
+              <div className="sponsor">
+                <img
+                  src={`${process.env.REACT_APP_API_BASE_URL}/${sponsor.image}`}
+                  alt={sponsor.name}
+                />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
