9f0280c56a12765e55bec49418f3661f70558a9d
Thibault Verin <thibaultverin@hotmail.com>
2021-01-27 20:27:45 +0100

    Fixing some CSS issues

--- src/components/Home/ScrollButton.js
+++ src/components/Home/ScrollButton.js
@@ -5,10 +5,7 @@ import './ScrollButton.scss';
 const ScrollButton = () => {
   return (
     <div>
-      <ScrollUpButton
-        ContainerClassName="scrollButton"
-        TransitionClassName="transitionedScrollButton"
-      />
+      <ScrollUpButton ContainerClassName="scrollButton" />
     </div>
   );
 };
